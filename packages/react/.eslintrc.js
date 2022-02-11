2e48cf845523a1a9111dd615a22e5f3ec98fe634
pochun-chiu <pochun.chiu@instill.tech>
2022-02-11 11:43:05 +0800

    feat: setup init folder structure

---
+++ packages/react/.eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require("keycap-config/eslint-preset");
---
+++ packages/react/src/components/Keycap.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface Props {}
+
+const Keycap: FC<Props> = () => {
+  return <div>hi</div>;
+};
+
+export default Keycap;
