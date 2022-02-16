2816ac6365a6d1c19d9d58be08d7bc98ac96a871
pochun-chiu <pochun.chiu@instill.tech>
2022-02-16 10:51:07 +0800

    feat: add keyboard layout

---
+++ packages/react/src/services/KeyboardLayout.ts
@@ -0,0 +1,11 @@
+export const getKeyboardLayout = () => {
+  return {
+    default: [
+      "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+      "{tab} q w e r t y u i o p [ ] \\",
+      "{lock} a s d f g h j k l ; ' {enter}",
+      "{shift} z x c v b n m , . / {shift}",
+      ".com @ {space}",
+    ],
+  };
+};
