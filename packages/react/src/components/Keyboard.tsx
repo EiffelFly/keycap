7c34b47db3981e3a4d31e5aa0bc215552a7b7422
pochun-chiu <pochun.chiu@instill.tech>
2022-02-25 10:41:22 +0800

    feat: add basic Keyboard structure and design

--- packages/react/src/components/Keyboard.tsx
+++ packages/react/src/components/Keyboard.tsx
@@ -1,25 +1,46 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { getKeyboardLayout } from "../services/KeyboardLayout";
+import { specialKeyDisplayName } from "../services/Utils";
 import "./css/Keyboard.css";
 
 export interface KeyboardProps {}
 
 const Keyboard: FC<KeyboardProps> = () => {
+  const keydownHandler = (e: KeyboardEvent) => {
+    console.log(e.key)
+    if (e.key === "n" && e.metaKey) {
+      console.log(e)
+      e.preventDefault();
+      e.stopPropagation();
+      alert("captured");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", keydownHandler, false);
+    return;
+  }, []);
+
   return (
     <div className="keycap-keyboard-container">
       {getKeyboardLayout().default.map((row, index) => (
         <div className="keycap-keyboard-row" key={`keycap-row-${index}`}>
-          {row.split(" ").map((keycap) => {
+          {row.split(" ").map((keycap, index) => {
+            const displayName = keycap.match(/{*.}/)
+              ? specialKeyDisplayName[keycap]
+              : keycap;
+
             return (
-              <div
+              <button
                 className={
                   keycap.match(/{*.}/)
                     ? "keycap-special-key"
                     : "keycap-standard-key"
                 }
+                key={`keycap-${keycap}-${index}`}
               >
-                {keycap}
-              </div>
+                {displayName}
+              </button>
             );
           })}
         </div>
