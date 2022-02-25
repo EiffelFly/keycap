062f3a13746a29a5358ff619485fdb6f76096a04
pochun-chiu <pochun.chiu@instill.tech>
2022-02-25 11:21:20 +0800

    feat: add general keybind type

---
+++ packages/core/src/type/general.ts
@@ -0,0 +1,21 @@
+export interface KeyBindInfo {
+  id: string;
+  name: string;
+  platform: "mac" | "window" | "linux";
+  browser: "chrome" | "webkit" | "firefox";
+  capturable: boolean;
+  /** If the keybind will only be valid under specific condition
+   * If the keybind don't make visual change but it still triggered, then 
+   * the keybind doesn't count as conditional. Like Meta+2, when there has
+   * no second tab, this key will still be triggered but without action, this
+   * key doesn't count as conditional
+  */
+  conditional: boolean; 
+  condition?: string; 
+}
+
+export interface BrowserKeybindSet {
+  mac: { [key: string]: KeyBindInfo };
+  chrome: { [key: string]: KeyBindInfo };
+  linux: { [key: string]: KeyBindInfo };
+}
