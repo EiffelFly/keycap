eb8747486b53e6a2c2445baa9751c286fa4a8a7a
pochun-chiu <pochun.chiu@instill.tech>
2022-03-02 11:32:26 +0800

    feat: successfully setup local workspace with type def

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -5,6 +5,7 @@ import {
   babel,
   typescript,
   resolve,
+  dts
 } from "../../rollup.config";
 
 export default [
@@ -12,19 +13,25 @@ export default [
     input: "src/index.ts",
     output: [
       {
-        file: `${packageJson.name}.js`,
+        file: packageJson.main,
         format: "cjs",
         strict: true,
         sourcemap: true,
         exports: "auto",
       },
       {
-        file: `${packageJson.name}.esm.js`,
+        file: packageJson.module,
         format: "esm",
         strict: true,
         sourcemap: true,
       },
     ],
     plugins: [resolve(), typescript(CONFIG_TYPESCRIPT), babel(CONFIG_BABEL)],
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    external: [/\.css$/],
+    plugins: [dts()],
   },
 ];
