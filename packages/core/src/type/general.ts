55f6f5bfb3b8dc390bdd49a0cfade072bf96f139
pochun-chiu <pochun.chiu@instill.tech>
2022-03-08 11:39:55 +0800

    refactor: refactor KeyBindInfo type

--- packages/core/src/type/general.ts
+++ packages/core/src/type/general.ts
@@ -5,14 +5,14 @@ export interface KeyBindInfo {
   browser: "chrome" | "webkit" | "firefox";
   capturable: boolean;
   /** If the keybind will only be valid under specific condition.
-   * For example, if the keybind don't make visual change but it still triggered, then 
+   * For example, if the keybind don't make visual change but it still triggered, then
    * the keybind doesn't count as conditional. Like Meta+2, when there has
    * no second tab, this key will still be triggered but without action, this
    * key doesn't count as conditional.
-  */
-  conditional: boolean; 
-  condition?: string; 
-  type: "browserDefault" | "userDefined" | "appPreset"
+   */
+  conditional: boolean;
+  condition?: string;
+  type: "browserDefault" | "userDefined" | "appPreset";
 }
 
 export interface BrowserKeybindSet {
