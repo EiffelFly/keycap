03dd80ca26cc439c44fdfcba29337be52ffbdb3a
pochun-chiu <pochun.chiu@instill.tech>
2022-03-18 11:41:19 +0800

    feat: add multiple key type list

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -141,3 +141,129 @@ export const getEventModifiers = (e: KeyboardEvent): Modifiers[] => {
 
   return modifiers;
 };
+
+const FUNCTION_KEY = [
+  "ContextMenu",
+  "Alt",
+  "Meta",
+  "Control",
+  "CapsLock",
+  "Shift",
+  "Tab",
+  "Escape",
+  "Esc",
+  "Backspace",
+  "Enter",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowRight",
+  "ArrowLeft",
+  "Space",
+  "Delete",
+  "End",
+  "Help",
+  "Home",
+  "Insert",
+  "PageDown",
+  "PageUp",
+  "PrintScreen",
+  "ScrollLock",
+  "Pause",
+];
+
+const NUMPAD_KEY = [
+  "NumLock",
+  "Numpad0",
+  "Numpad2",
+  "Numpad3",
+  "Numpad4",
+  "Numpad5",
+  "Numpad6",
+  "Numpad7",
+  "Numpad8",
+  "Numpad9",
+  "NumpadAdd",
+  "NumpadComma",
+  "NumpadDecimal",
+  "NumpadDivide",
+  "NumpadEnter",
+  "NumpadEqual",
+  "NumpadMultiply",
+  "NumpadSubtract",
+];
+
+const NON_ALPHABET_KEY = [
+  "Comma",
+  "Period",
+  "Semicolon",
+  "Quote",
+  "BracketLeft",
+  "BracketRight",
+  "Backquote",
+  "Backslash",
+  "Minus",
+  "Equal",
+  "IntlRo",
+  "IntlYen",
+];
+
+const ALPHABET_KEY = [
+  "Digit1",
+  "Digit2",
+  "Digit3",
+  "Digit4",
+  "Digit5",
+  "Digit6",
+  "Digit7",
+  "Digit8",
+  "Digit9",
+  "Digit0",
+  "KeyA",
+  "KeyB",
+  "KeyC",
+  "KeyD",
+  "KeyE",
+  "KeyF",
+  "KeyG",
+  "KeyH",
+  "KeyI",
+  "KeyJ",
+  "KeyK",
+  "KeyL",
+  "KeyM",
+  "KeyN",
+  "KeyO",
+  "KeyP",
+  "KeyQ",
+  "KeyR",
+  "KeyS",
+  "KeyT",
+  "KeyU",
+  "KeyV",
+  "KeyW",
+  "KeyX",
+  "KeyY",
+  "KeyZ",
+];
+
+export const getMatchedKey = (e: KeyboardEvent) => {
+  if (e.key) {
+    if (FUNCTION_KEY.includes(e.key)) {
+      return getFunctionKey(e);
+    }
+
+    if (NON_ALPHABET_KEY.includes(e.key)) {
+      return getNonAlphabetKey(e);
+    }
+
+    if (NUMPAD_KEY.includes(e.key)) {
+      return getNumpagKey(e);
+    }
+
+    if (ALPHABET_KEY.includes(e.key)) {
+      return getAlphabetKey(e);
+    }
+  }
+};
+
+const getFunctionKey = (e: KeyboardEvent) => {};
