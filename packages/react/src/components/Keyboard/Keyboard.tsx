87dc417c8f5f0ac974df7d9704a359ac7d865c78
pochun-chiu <pochun.chiu@instill.tech>
2022-03-27 20:16:42 +0800

    refactor: refactor Keyboard component to use new Keycap component and useKeycap

--- packages/react/src/components/Keyboard/Keyboard.tsx
+++ packages/react/src/components/Keyboard/Keyboard.tsx
@@ -1,60 +1,26 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { getKeyboardLayout } from "../../services/KeyboardLayout";
-import { specialKeyDisplayName } from "../../services/Utils";
 import "./Keyboard.css";
-import * as classNames from "classnames";
-import { getMatchedKeys } from "@keycap/core";
-
+import { useKeycap } from "../../hooks/useKeycap";
+import { Keycap } from "../Keycap";
 export interface KeyboardProps {}
 
 export const Keyboard: FC<KeyboardProps> = () => {
-  // const [currentKeybinds, setCurrentKeybinds] = useState<KeyBindInfo[]>();
-  // const [currentFunctionKeySet, setCurrentFunctionKeySet] = useState<string>();
-  // const [onCtrl, setOnCtrl] = useState(false);
+  const { keybinds } = useKeycap("chrome", "mac");
 
-  const keydownHandler = (e: KeyboardEvent) => {
-    console.log("keydown", e);
-
-    const { value } = getMatchedKeys("mac", "chrome", e);
-
-    console.log(value);
-  };
-
-  const keyupHandler = (e: KeyboardEvent) => {
-    console.log("keyup", e);
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", keydownHandler, false);
-    document.addEventListener("keyup", keyupHandler, false);
-    return () => {
-      document.removeEventListener("keydown", keydownHandler);
-      document.removeEventListener("keyup", keyupHandler);
-    };
-  }, []);
+  console.log(keybinds);
 
   return (
     <div className="keycap-keyboard-container">
       {getKeyboardLayout().default.map((row, index) => (
         <div className="keycap-keyboard-row" key={`keycap-row-${index}`}>
-          {row.split(" ").map((keycap, index) => {
-            const displayName = keycap.match(/{*.}/)
-              ? specialKeyDisplayName[keycap]
-              : keycap;
-
-            return (
-              <button
-                className={classNames.default(
-                  keycap.match(/{*.}/)
-                    ? "keycap-special-key"
-                    : "keycap-standard-key"
-                )}
-                key={`keycap-${keycap}-${index}`}
-              >
-                {displayName}
-              </button>
-            );
-          })}
+          {row.split(" ").map((keycap, index) => (
+            <Keycap
+              key={`keycap-${keycap}-${index}`}
+              isTarget={false}
+              keycap={keycap}
+            />
+          ))}
         </div>
       ))}
     </div>
